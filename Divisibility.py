f1982060607e96b4f3b4cbab7cd1c10ecb297ce6
HARISREE SURESH <getharisree@outlook.com>
2023-02-13 16:50:29 +0530

    Update Divisibility.py

--- Divisibility.py
+++ Divisibility.py
@@ -1,3 +1,12 @@
 N = int(input())
 data = [int(x) for x in input().split()]
 l = []
+
+for i,n in enumerate(nums):
+  l.append(n%19)
+c =int("".join([str(i) for i in l]))
+
+if c%10 == 0:
+  return(True)
+else:
+  return(False)
