ae19ec5de0336f08143e93eb9462519ab91c99be
HARISREE SURESH <getharisree@outlook.com>
2023-02-14 17:04:52 +0530

    Update Ali and Helping innocent people.py

--- Ali and Helping innocent people.py
+++ Ali and Helping innocent people.py
@@ -2,7 +2,6 @@ n = input()
 vow = ["A","E","I","O","U","Y"]
 
 
-
 if n[2] not in vow:
     if n[0].isnumeric() and n[1].isnumeric() and n[3].isnumeric() and n[4].isnumeric() and n[5].isnumeric() and n[7].isnumeric() and n[8].isnumeric():
         if (int(n[0]) + int(n[1]))%2 == 0  and (int(n[3]) + int(n[4]))%2 == 0 and (int(n[4]) + int(n[5]))%2 == 0 and (int(n[7]) + int(n[8]))%2 == 0 :
