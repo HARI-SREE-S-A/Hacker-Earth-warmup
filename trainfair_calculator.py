dc63534b1464c6251e6ca4e274da812d802ac8f8
HARISREE SURESH <getharisree@outlook.com>
2023-02-22 20:50:41 +0530

    Update trainfair_calculator.py

--- trainfair_calculator.py
+++ trainfair_calculator.py
@@ -1,1 +1,34 @@
 ### cirular list concept tram ride
+station = int(input())
+start = int(input())
+finish = int(input())
+charges =[int(x) for x in input().split()]
+circle = list(range(1,station+1))
+journey = list(range(start,finish))
+length  = len(journey)
+count = []
+countt = []
+index_strt = circle.index(start)
+f = index_strt
+r = f
+
+for i in range(0,len(circle)+1):
+    
+    if circle[r] != finish:
+        count.append(charges[r])
+        r += 1
+        if r > (len(circle)-1):
+            r = 0
+    else:
+        break
+
+for j in range(0,len(circle)+1):
+    if circle[f] != finish:
+        
+        countt.append(charges[f-1])
+        f -= 1
+a,b = sum(count),sum(countt)
+#print(count)
+#print(countt)
+print(min(a,b))
+
