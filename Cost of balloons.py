dfd591c66190caf1015526bbbcb3417695fc2568
HARISREE SURESH <getharisree@outlook.com>
2023-02-28 19:20:32 +0530

    Update Cost of balloons.py

--- Cost of balloons.py
+++ Cost of balloons.py
@@ -1,1 +1,16 @@
+dictn = {}
+a = []
+l = []
+for k in range(3):
+    dictn[k] = [int(x) for x in input().split()]
+for p in range(1):
+    for b in range(3):
+        a.append(dictn[b][p])
+r = 1
+for c in range(3):
+    l.append(dictn[c][r])
+a_c = a.count(1)
+b_c = l.count(1)
+print(a_c,b_c)
+
 
