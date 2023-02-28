98cb7945c0fb877ad5ea07be1c4223f926494c59
HARISREE SURESH <getharisree@outlook.com>
2023-02-28 18:49:01 +0530

    Update facebook_pic_upload.py

--- facebook_pic_upload.py
+++ facebook_pic_upload.py
@@ -5,13 +5,17 @@ dictn = {}
 for _ in range(N):
     dictn[_]  = [int(x) for x in input().split()]
 
+    
 for k in range(N):
     b = dictn[k]
    
     if b[0] or b[1] < L:
         print(b[0],b[1])
         print("UPLOAD ANOTHER")
+        
     elif b[0] and b[1] >= L:
+        
+        
         if b[0] == b[1]:
             print("ACCEPTED")
         else:
