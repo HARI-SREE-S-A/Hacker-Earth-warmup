d068bbe9bdbe0c1fcb27ac63cba3a30696df4a76
HARISREE SURESH <getharisree@outlook.com>
2023-03-01 16:27:42 +0530

    Update finding_effective_vaccine.py

--- finding_effective_vaccine.py
+++ finding_effective_vaccine.py
@@ -1,15 +1,21 @@
-vi = "AAGCCU"
+vi = list("AAGCCU")
 
 
-va = "BBGCUU"
+va = list("BBGCUU")
 vb = "CCGGUU"
 vil =[1]*2
 val = [1]*2
 vbl = [1]*2
 
-vil[0] = vi.count("C") 
-vil[1] = vi.count("G")
+virus_list = (vi.count("C"))* "C" + (vi.count("G") * ("G" ))
+virus_list = list(virus_list)
+print(virus_list)
 
+for n in va:
+    if n == "C":
+        virus_list.remove("C")
+    elif n == "G":
+        virus_list.remove("G")
 
 val[0] = va.count("C")
 val[1]= va.count("G")
@@ -17,7 +23,4 @@ val[1]= va.count("G")
 vbl[0] = vb.count("C")
 vbl[1]= vb.count("G")
 
-print(vil)
-print(val)
-print(vbl)
-
+virus_list
