d62dcce0efe7e037d93f78f6188b989fd36e6bfb
HARISREE SURESH <getharisree@outlook.com>
2023-03-01 15:32:07 +0530

    Update finding_effective_vaccine.py

--- finding_effective_vaccine.py
+++ finding_effective_vaccine.py
@@ -1,1 +1,23 @@
+vi = "AAGCCU"
+
+
+va = "BBGCUU"
+vb = "CCGGUU"
+vil =[1]*2
+val = [1]*2
+vbl = [1]*2
+
+vil[0] = vi.count("C") 
+vil[1] = vi.count("G")
+
+
+val[0] = va.count("C")
+val[1]= va.count("G")
+
+vbl[0] = vb.count("C")
+vbl[1]= vb.count("G")
+
+print(vil)
+print(val)
+print(vbl)
 
