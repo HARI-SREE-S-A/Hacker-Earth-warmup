ce52919baf773f7889465eb4b9a7e0f9f38c8eb6
HARISREE SURESH <getharisree@outlook.com>
2023-03-01 18:30:23 +0530

    Update finding_effective_vaccine.py

--- finding_effective_vaccine.py
+++ finding_effective_vaccine.py
@@ -1,3 +1,4 @@
+dictn = {}
 vi = list("AAGCCU")
 
 
@@ -9,18 +10,29 @@ vbl = [1]*2
 
 virus_list = (vi.count("C"))* "C" + (vi.count("G") * ("G" ))
 virus_list = list(virus_list)
-print(virus_list)
 
-for n in va:
-    if n == "C":
-        virus_list.remove("C")
-    elif n == "G":
-        virus_list.remove("G")
+for b in range(2):
+    virus_listss = (vi.count("C"))* "C" + (vi.count("G") * ("G" ))
+    virus_lists  = list(virus_listss)
+    
+    
+    va = [str(x) for x in input()]
+    for n in va:
+       
+        if n == "C" and "C" in virus_lists:
+            #print(virus_lists)
+            virus_lists.remove("C")
+        elif n == "G" and "G" in virus_lists:
+            #print(virus_lists)
+            virus_lists.remove("G")
+        dictn[b] = len(virus_lists)
+        
+    
 
-val[0] = va.count("C")
-val[1]= va.count("G")
 
-vbl[0] = vb.count("C")
-vbl[1]= vb.count("G")
-
-virus_list
+   
+print(dictn)
+nl = [1]*2
+for k in range(2):
+    nl[k] = dictn[k]
+print( ( nl.index(max(nl)) )+1 )
