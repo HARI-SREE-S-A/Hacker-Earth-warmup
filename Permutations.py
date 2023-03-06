a3a1f07dee7ee4cc3ffdb993b705e74b3d3ef419
HARISREE SURESH <getharisree@outlook.com>
2023-03-06 21:46:14 +0530

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -2,18 +2,24 @@ c = [int(y) for y in input().split()]
 j = c[1]
 per = [int(z) for z in input().split()]
 
-
+l = []
+b  = per
 for _ in range(j):
     
     p = per
     
     k = [int(x) for x in input().split()]
     
-    for i in range(k[0],k[1]):
+    for i in range(k[0],k[1]+1):
         try:
-            p.remove(per[i])
+            l.append(per[i])
         except IndexError:
             None
-    print(max(p))
-        
-    
+    print(l)
+    for n in l:
+        print(n)
+        if n in per:
+            b.remove(n)
+        else:
+            pass
+    print((b))
