de4923a9f45031eea876ce05a2ad925819ab4212
HARISREE SURESH <getharisree@outlook.com>
2023-03-06 16:10:54 +0530

    Update common-integer that can divide the numbers(method1).py

--- common-integer that can divide the numbers(method1).py
+++ common-integer that can divide the numbers(method1).py
@@ -1,1 +1,12 @@
+nums = [int(x) for x in input().split()]
+m = max(nums)
+n = min(nums)
+result = []
+for i in range(1,m):
+    print(n,m,i)
+    if nums[0] % i == nums[1] % i == 0:
+        print(i)
+        result.append(i)
+print(len(result))
+
 
