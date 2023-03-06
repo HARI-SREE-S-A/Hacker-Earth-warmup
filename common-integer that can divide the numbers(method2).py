e7d8656cf439c54208fbdadbef626cd8bd5385db
HARISREE SURESH <getharisree@outlook.com>
2023-03-06 16:11:51 +0530

    Update common-integer that can divide the numbers(method2).py

--- common-integer that can divide the numbers(method2).py
+++ common-integer that can divide the numbers(method2).py
@@ -1,1 +1,12 @@
-
+from fractions import gcd
+def cf(num1,num2):
+    n=[]
+    g=gcd(num1, num2)
+    for i in range(1, g+1): 
+        if g%i==0: 
+            n.append(i)
+    return n
+nums = [int(x) for x in input().split()]
+cf(nums[0],nums[1])
+result = n
+print(len(result)
