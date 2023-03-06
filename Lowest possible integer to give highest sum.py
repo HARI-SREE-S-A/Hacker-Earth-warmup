88c034c2b284304173e5d4266581b3ba74eb83cf
HARISREE SURESH <getharisree@outlook.com>
2023-03-06 16:08:45 +0530

    Update Lowest possible integer to give highest sum.py

--- Lowest possible integer to give highest sum.py
+++ Lowest possible integer to give highest sum.py
@@ -1,1 +1,11 @@
+length = int(input())
+nums = sorted([int(x) for x in input().split()])
+n_sum = sum(nums)
 
+for n in nums:
+    numm = n * length
+    
+
+    if numm> n_sum:
+        print(n)
+        break
